9e50aff066288763f789a02f198f2f7faf5f43a2
Arun <arunjatharakonda03@gmail.com>
2025-12-16 15:31:24 +0530

    Add files via upload

---
+++ data types.py
@@ -0,0 +1,13 @@
+a = 10          # integer
+b = 2.5         # float
+c = "Hello"     # string
+d = True        # boolean
+e = [1,2,3]     #list
+
+
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
